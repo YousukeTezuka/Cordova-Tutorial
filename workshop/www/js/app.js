c552ecfdeb18a5e513651cde3060377e052f7a69
YousukeTezuka <YousukeTezuka@gmail.com>
2015-04-21 18:15:37 +0900

    Finish Module9

--- workshop/www/js/app.js
+++ workshop/www/js/app.js
@@ -5,11 +5,13 @@
     var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+    HomeView.prototype.template = Handlebars.compile($('#home-tpl').html());
+    EmployeeListView.prototype.template = Handlebars.compile($('#employee-list-tpl').html());
+
     var service = new EmployeeService();
     service.initialize().done(function () {
-        renderHomeView();
+        $('body').html(new HomeView(service).render().$el);
     });
-
     /* --------------------------------- Event Registration -------------------------------- */
     document.addEventListener('deviceready', function () {
       StatusBar.overlayWebView( false );
@@ -17,15 +19,5 @@
       StatusBar.styleDefault();
     }, false);
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            $('.content').html(employeeListTpl(employees));
-        });
-    }
-
-    function renderHomeView() {
-        $('body').html(homeTpl());
-        $('.search-key').on('keyup', findByName);
-    }
 
 }());
